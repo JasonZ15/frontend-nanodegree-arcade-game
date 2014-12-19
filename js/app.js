aa9675b0486371dc24dc99accada6584e947ebbd
JasonZ15 <vines15@gmail.com>
2014-12-19 13:27:56 -0800

    generate Enemy objects through Enemy class
    set number of enemies and give them different speed

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(spd) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
+    this.x = -100;
+    this.y = 0;
+    this.speed = spd;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -14,6 +16,8 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x = this.x + this.speed * dt;
+    if(this.x > 505) {this.x = -100;}
 }
 
 // Draw the enemy on the screen, required method for game
@@ -24,13 +28,31 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+  this.x = 0;
+  this.y = 0;
+  this.sprite = 'images/char-boy.png';
+}
+Player.prototype.handleInput = function() {
 
+}
+Player.prototype.update = function() {
+
+}
+Player.prototype.render = function() {
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
-
+var allEnemies = [];
+var numEnemies = 3
+for (var i = 0; i < numEnemies; i++) {
+  var mySpd = Math.round(Math.random() * 300)
+  allEnemies.push(new Enemy(mySpd));
+}
+var player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
